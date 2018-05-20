e3e3c84bdbbf2e4abedf60647079281c681c6f8c
DESKTOP-LSDTIAF\Shahid Bangash <shahid@smsami.com>
2018-05-21 01:38:24 +0500

    module and component setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SmCalendarModule } from './sm-calendar/sm-calendar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SmCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
